db49b6c64d0ee75e4ab7b3cd4cfba96488d9bbf3
vagrant <vagrant@homestead>
2018-02-13 09:19:10 +0000

    Add account activations

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -5,18 +5,33 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Http\Requests;
 use App\Models\User;
+use Mail;
+use Auth;
+
 
 class UsersController extends Controller
 {
   public function __construct()
    {
        $this->middleware('auth', [
-           'except' => ['show', 'create', 'store','index']
+           'except' => ['show', 'create', 'store','index','confirmEmail']
        ]);
        $this->middleware('guest', [
            'only' => ['create']
        ]);
    }
+   public function confirmEmail($token)
+    {
+        $user = User::where('activation_token', $token)->firstOrFail();
+
+        $user->activated = true;
+        $user->activation_token = null;
+        $user->save();
+
+        Auth::login($user);
+        session()->flash('success', '恭喜你，激活成功！');
+        return redirect()->route('users.show', [$user]);
+    }
     public function create()
     {
         return view('users.create');
@@ -29,19 +44,33 @@ class UsersController extends Controller
     public function store(Request $request)
     {
       $this->validate($request, [
-             'name' => 'required|max:50',
-             'email' => 'required|email|unique:users|max:255',
-             'password' => 'required|confirmed|min:6'
-         ]);
+            'name' => 'required|max:50',
+            'email' => 'required|email|unique:users|max:255',
+            'password' => 'required|confirmed|min:6'
+        ]);
 
-         $user = User::create([
-             'name' => $request->name,
-             'email' => $request->email,
-             'password' => bcrypt($request->password),
-         ]);
-         Auth::login($user);
-         session()->flash('success', '欢迎，您将在这里开启一段新的旅程~');
-         return redirect()->route('users.show', [$user]);
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password),
+        ]);
+
+        $this->sendEmailConfirmationTo($user);
+        session()->flash('success', '验证邮件已发送到你的注册邮箱上，请注意查收。');
+        return redirect('/');
+    }
+    protected function sendEmailConfirmationTo($user)
+    {
+        $view = 'emails.confirm';
+        $data = compact('user');
+        $from = 'aufree@yousails.com';
+        $name = 'Aufree';
+        $to = $user->email;
+        $subject = "感谢注册 Sample 应用！请确认你的邮箱。";
+
+        Mail::send($view, $data, function ($message) use ($from, $name, $to, $subject) {
+            $message->from($from, $name)->to($to)->subject($subject);
+        });
     }
     public function edit(User $user)
     {
--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -12,14 +12,15 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        $users = factory(User::class)->times(50)->make();
-        User::insert($users->makeVisible(['password', 'remember_token'])->toArray());
+       $users = factory(User::class)->times(50)->make();
+       User::insert($users->makeVisible(['password', 'remember_token'])->toArray());
 
-        $user = User::find(1);
-        $user->name = 'Aufree';
-        $user->email = 'aufree@yousails.com';
-        $user->password = bcrypt('password');
-        $user->is_admin = true;
-        $user->save();
+       $user = User::find(1);
+       $user->name = 'Aufree';
+       $user->email = 'aufree@yousails.com';
+       $user->password = bcrypt('password');
+       $user->is_admin = true;
+       $user->activated = true;
+       $user->save();
     }
 }
