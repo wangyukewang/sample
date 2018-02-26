d2551aa809c777261b4ffaebbc7d15f766bee541
vagrant <vagrant@homestead>
2018-02-26 13:27:15 +0000

    fixed bug

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -63,8 +63,8 @@ class UsersController extends Controller
     {
         $view = 'emails.confirm';
         $data = compact('user');
-        $from = 'aufree@yousails.com';
-        $name = 'Aufree';
+        $from = '827438419@qq.com';
+        $name = '827438419@qq.com';
         $to = $user->email;
         $subject = "感谢注册 Sample 应用！请确认你的邮箱。";
 
